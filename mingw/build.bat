2ae06e2d6e9e05db55450e5c5bded9fd7f856712
Ido.Yehieli <Ido.Yehieli@23ac1a0c-1b45-0410-8986-4bdf4f9b9510>
2008-02-02 21:59:55 +0000

    changed compilation flag for windows

--- mingw/build.bat
+++ mingw/build.bat
@@ -2,5 +2,5 @@ REM mingw build script.
 REM pdcurses.h should be in your include path (e.g. /usr/include/mingw).
 
 cd..
-gcc -std=c99 -DWIN32 -mno-cygwin -O2 -lm cr.c mingw/pdcurses.a -o cr
+gcc -std=c99 -mno-cygwin -O2 -lm cr.c mingw/pdcurses.a -o cr
 strip cr.exe
