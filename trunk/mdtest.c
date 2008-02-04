5ffd4fb64053fef44e8f6bacbed279a2854bfa0b
Ido.Yehieli <Ido.Yehieli@23ac1a0c-1b45-0410-8986-4bdf4f9b9510>
2008-02-04 09:15:37 +0000

    temporary adding mdtest

---
+++ trunk/mdtest.c
@@ -0,0 +1,33 @@
+#include <ncurses.h>
+
+int errs;
+void init_curses() {
+	errs=0;
+	if (ERR==keypad(stdscr=initscr(),true))
+		mvaddstr(errs++,0,"Cannot enable keypad");
+	if (ERR==noecho())
+		mvaddstr(errs++,0,"Cannot set noecho mode");
+	if (ERR==curs_set(0))
+		mvaddstr(errs++,0,"Cannot set invisible cursor");
+	if (ERR==start_color() || !has_colors())
+		mvaddstr(errs++,0,"Cannot enable colors");
+	else {
+		for (int c=0; c<8; c++)
+			init_pair(c,c,0);
+	}
+}
+void print_info(int c) {
+	int msgs=errs;
+	char msg[50];
+	sprintf(msg, "c: %d     ",c);
+	mvaddstr(msgs++,0,msg);
+}
+int main (){
+	init_curses();
+	int c;
+	while(c!=3){
+		print_info(c=md_readchar(stdscr));
+	}
+	return endwin();
+
+}
